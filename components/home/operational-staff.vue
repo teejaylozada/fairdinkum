ecb9e0bac88a7b77a42d644360cc895501971e1d
teejaylozada <timothyjohn.lozada@hmr.ph>
2023-11-15 08:29:49 +0800

    finalize update

--- components/home/operational-staff.vue
+++ components/home/operational-staff.vue
@@ -13,27 +13,27 @@
             Our current staff levels vary as required, but are approximately 110 staff, 50% skilled tradesman with the balance being semi-skilled laborers and trainees, inclusive of:
           </p>
           <div class="grid grid-cols-1 sm:grid-cols-2 gap-2 lg:gap-4">
-            <div class="custom-button-container bg-orange-500 hover:bg-orange-600  py-2 px-4 rounded transition duration-300">
+            <div class="custom-button-container bg-gray-100   py-2 px-4 rounded transition duration-300">
                 Mechanical Engineeers
             </div>
-            <div class="custom-button-container bg-orange-500 hover:bg-orange-600  py-2 px-4 rounded transition duration-300">
+            <div class="custom-button-container bg-gray-100   py-2 px-4 rounded transition duration-300">
                 Industrial Engineeers
             </div>
-            <div class="custom-button-container bg-orange-500 hover:bg-orange-600  py-2 px-4 rounded transition duration-300">
+            <div class="custom-button-container bg-gray-100   py-2 px-4 rounded transition duration-300">
                 Quality Assurance
             </div>
-            <div class="custom-button-container bg-orange-500 hover:bg-orange-600  py-2 px-4 rounded transition duration-300">
+            <div class="custom-button-container bg-gray-100   py-2 px-4 rounded transition duration-300">
                 Quality Surveyor
             </div>
           </div>
           <div class="grid grid-cols-1 sm:grid-cols-2 gap-2 lg:gap-4 mt-2">
-            <div class="custom-button-container bg-orange-500 hover:bg-orange-600  py-2 px-4 rounded transition duration-300">
+            <div class="custom-button-container bg-gray-100   py-2 px-4 rounded transition duration-300">
                 Welders / Fabricators
             </div>
-            <div class="custom-button-container bg-orange-500 hover:bg-orange-600  py-2 px-4 rounded transition duration-300">
+            <div class="custom-button-container bg-gray-100   py-2 px-4 rounded transition duration-300">
                 Machine Operators
             </div>
-            <div class="custom-button-container bg-orange-500 hover:bg-orange-600  py-2 px-4 rounded transition duration-300">
+            <div class="custom-button-container bg-gray-100   py-2 px-4 rounded transition duration-300">
                 Safety Officers
             </div>
           </div>
--- pages/executive-summary/index.vue
+++ pages/executive-summary/index.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Row1 -->
-  <div class="p-4 lg:p-10 grid grid-cols-1 lg:grid-cols-2 gap-4">
+  <!-- <div class="p-4 lg:p-10 grid grid-cols-1 lg:grid-cols-2 gap-4">
     <div class="px-10 lg:py-0 flex flex-col lg:text-left">
       <h3 class="px-5 lg:px-0 text-orange-500 text-lg font-semibold mb-2">Executive Summary</h3>
       <h1 class="px-5 lg:px-0 text-xl lg:text-3xl font-bold mb-2 lg:mb-4">Experience and Innovation in Metal Fabrication</h1>
@@ -26,7 +26,42 @@
         The Fairdinkum strategy is to expand and develop within the parameters of the market requirements. i.e. if the market has a requirement for a particular product, and the development of this product is sustainable in terms of ROI in regards to Capital Investment requirements, FDF have the human resources available with the necessary skillsets to perform the task. FDF will procure the necessary equipment and train sufficient additional staff to continue the development and maintain the operation.
       </p>
     </div>
+  </div> -->
+  <div class="p-4 lg:p-10 grid grid-cols-1 lg:grid-cols-1 gap-4">
+  <div class="lg:col-span-1 lg:flex lg:flex-col lg:justify-between">
+    <div class="px-10 lg:text-left">
+      
+      <h3 class="text-orange-500 text-lg font-semibold lg:px-0 ">Executive Summary</h3>
+      <div class="lg:col-span-1 relative lg:p-1 flex justify-center items-center m-12" style="float:right;width:500px;height:200px;">
+          <div class="w-full h-auto border-white border-4 rounded-lg overflow-hidden">
+            <img src="~/assets/executive-summary-4x4.png" alt="Image 1" class="w-3/4 md:w-2/3 lg:w-3/4 h-auto object-cover mx-auto">
+            <img src="~/assets/executive-summary2.png" alt="Image 2" class="w-1/3 h-auto border-white border-4 absolute top-20 lg:top-1/4 left-0 lg:left-0 transform translate-x-0 translate-y-1/5 z-20">
+          </div>
+        </div>
+      <h1 class="text-xl lg:text-3xl font-bold mb-2 lg:mb-4 lg:px-0 px-5">Experience and Innovation<br> in Metal Fabrication</h1>
+      <p>
+
+        FAIRDINKUM FABRICATORS INC (Formally HMR Fabricators) was established in Angono in 1985 initially to refurbish and fabricate shelving and Pallet Racking for HMR Philippines. In 2011, the operations were expanded to provide services to the Construction, Geological, Exploration, and Mining industries, and hence the development of Fairdinkum Fabricators.
+        <br>
+        <br>
+        The company continues to refurbish the shelving and pallet racking, while in parallel servicing the requirements for external Fabrication. Since the conception of the company FDF has emphasized delivering quality, reliable services, and materials to the industry.
+        <br>
+        <br>
+        All the products and services that FDF provide have been the best available in the market and fabricated directly to our clients' requirements and specifications with the necessary backup the industry demands.
+        <br>
+        <br>
+        FDF emphasizes a thorough understanding of our client's project requirements by maintaining close communication at all times so as to derive a complete understanding of each project FDF is involved in. During the course of our involvement, FDF undertakes a hands-on approach to working with our clients and assisting them in varying ways to ensure the project progresses as programmed and with no delays, where achievable.
+ 
+        <br>
+        <br>
+        The Fairdinkum strategy is to expand and develop within the parameters of the market requirements. i.e., if the market has a requirement for a particular product, and the development of this product is sustainable in terms of ROI regarding Capital Investment requirements, FDF has the human resources available with the necessary skill sets to perform the task. FDF will procure the necessary equipment and train sufficient additional staff to continue the development and maintain the operation.
+        </p>
+    </div>
   </div>
+</div>
+
+
+
 
 
   <!-- Row2 -->
@@ -49,7 +84,8 @@
     </div>
   </div>
   <!-- row3 -->
-  <div class="bg-gray-200">
+  <OperationalStaff />
+  <!-- <div class="bg-gray-200">
   <div class="p-4 lg:p-10 grid grid-cols-1 lg:grid-cols-2 gap-4">
       <div class="px-10 flex flex-col">
         <div class="lg:hidden flex flex-col px-4 lg:px-8 py-6 lg:py-10">
@@ -97,7 +133,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
   <!-- row4 -->
   <div class="p-4 lg:p-10 grid grid-cols-1 lg:grid-cols-2 gap-4">
     <div class="px-10 flex flex-col lg:text-left">
@@ -130,7 +166,7 @@
 </template>
 
 <script setup lang="ts">
-
+import OperationalStaff from "@/components/home/operational-staff.vue"
 </script>
 
 <style scoped>
