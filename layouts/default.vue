7da40c163001e79fc182cfd2fc515579549bb61b
teejaylozada <timothyjohn.lozada@hmr.ph>
2023-11-30 13:57:26 +0800

    Update remove Footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -77,23 +77,7 @@
         </div>
 
         <!-- Column 4: Address and Logo -->
-        <div class="footer-column" style="overflow: hidden;">
-          <h1 class="text-xl font-bold mb-4">Contact Us</h1>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3867.500537091069!2d121.109457!3d14.2239983!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd62e3ce3bbf47%3A0xdff686c49486a9c!2sSilangan%20Industrial%20Park-Calamba!5e0!3m2!1sen!2sph!4v1697365276678!5m2!1sen!2sph" 
-            width="350" 
-            height="200" 
-            style="border:0;" 
-            allowfullscreen="" 
-            loading="lazy" 
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
-          <p class="text-gray-300">
-            HMR Environmental Compound, Silang Industrial Estate Canlubang, Calamba, Laguna, Philippines 4028
-          </p>
-        </div>
-      </div>
-
-      <div class="text-center">
-        <p>©2023. All Rights Reserved. Designed by <span class="text-custom-orange font-semibold">HPAA</span></p>
+        <OurMiniMap />
       </div>
     </footer>
   </div>
@@ -101,6 +85,7 @@
 
 <script setup>
 import '@fortawesome/fontawesome-free/css/all.css';
+import OurMiniMap from "@/components/our-mini-map.vue"
 import { ref, onUnmounted } from 'vue';
 
 const isMenuOpen = ref(false);
